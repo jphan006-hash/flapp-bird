98a27d9078987a0054603b223bee27687b0e2acf
jphan006-hash <jphan006@somerset.colegia.org>
2026-03-11 15:15:36 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const bird = document.getElementById('bird');
+let birdTop = 250;
+let gravity = 2;
+
+function gameLoop() {
+    birdTop += gravity;
+    bird.style.top = birdTop + 'px';
+    requestAnimationFrame(gameLoop);
+}
+
+window.addEventListener('keydown', () => {
+    birdTop -= 50; // Jump height
+});
+
+gameLoop();
